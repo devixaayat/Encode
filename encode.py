71068637e5aea03f46ae636ab3bb53215eb7a298
D3VI R9JPOOT <devixaayat@gmail.com>
2024-09-22 08:05:47 +0500

    Add files via upload

---
+++ encode.py
@@ -0,0 +1,21 @@
+import os
+logo = '\x1b[1;36m\██████╗ ███████╗██╗   ██╗██╗██╔══██╗██╔════╝██║   ██║██║██║  ██║█████╗  ██║   ██║██║██║  ██║██╔══╝  ╚██╗ ██╔╝██║██████╔╝███████╗ ╚████╔╝ ██║╚═════╝ ╚══════╝  ╚═══╝  ╚═╝       '
+os.system('clear')
+    print(logo)
+    print('[A] Decode Zlib \n[B] Decode Base64')
+    choice = input('[✓] input:')
+    dec()
+
+def dec():
+    os.system('clear')
+    print(logo)
+    print(' Example file_enc.py')
+    file = input(' input file: ')
+    ogge = str(open(file, 'r').read())
+    data = ogge.replace('exec', 'MrDeviRajput=')
+    data2 = f"""{data}\nopen("Fuck_done.py","w").write(MrDevirajpoot.decode('utf-8')) """
+    open('.dara.py', 'w').write(data2)
+    os.system('python .dara.py')
+    print(' file saved with Fuck_done.py')
+    print(' Dec done [✓] ')
+mahadi()
